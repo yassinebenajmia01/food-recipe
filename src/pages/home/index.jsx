ea9d987fee28d47e52b2961137e72ffa9dc2a36c
yassinebenajmia01 <yacineajmiaskip7530@gmail.com>
2024-12-24 22:05:14 +0100

    24 decembre

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -14,7 +14,7 @@ export default function Home() {
       ) : (
         <div>
           <p className="lg:text-4xl text-xl text-center text-black font-extrabold">
-            Nothing to show. Please search something
+            Nothing to show . Please search something
           </p>
         </div>
       )}
